6f65684e7ddd2413a6d2deff50e11b5d101a04db
Hamdanafif <hamdanafiftsm@gmail.com>
2023-06-23 02:40:52 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo "<h1>Hay Perkenalkan</h1>";
+        echo" Nama saya Hamdan Afif Bancin";
+        echo"<p>Saya tinggal di Tangerang Selatan</p>";
+        echo "<p>Pada saat ini saya sedang belajar koding</p>";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Hamdan </li>
+ <li>Nama Tengah : Afif </li>
+ <li>Nama Belakang : Bancin</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Tangerang selatan</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tangerang</li>
+ </ul>
+ <h4>Hobby</h4>
+ <ul type="square">
+ <li>Swimming</li>
+ <li>Touring</li>
+ </ul>
+</section>
